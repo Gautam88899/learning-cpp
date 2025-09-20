ea7925c4992b64836b4549328bf4e4a3ad34db00
Gautam88899 <gautam787075@gmail.com>
2025-09-20 21:20:31 +0530

    patter program in c++

---
+++ pattern4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int i,j,n;
+    cout<<"enter the valur of n";
+    cin>>n;
+    for(i=1; i<=n;i++)
+    {
+        for(j=1;j<=n-i+1;j++){
+            cout<<j<<" ";
+
+
+        }
+        cout<<endl;
+
+
+        
+    }
+}
