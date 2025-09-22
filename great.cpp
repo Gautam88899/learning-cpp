9fb1babca972dd81cf5a8699b309189959363c07
Gautam88899 <gautam787075@gmail.com>
2025-09-22 15:38:40 +0530

    function in c++

---
+++ great.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace  std;
+int maximum(int num1,int num2){
+
+
+
+    if(num1>num2){
+        cout<<"num1 is greater ";
+    }
+    else{
+        cout<<"num2  is grater";
+
+
+        
+
+    }
+    
+}
+ int main ()
+
+ {
+ 
+    int  num1,num2;
+    cout<<"enter the first number ";
+    cin>>num1;
+    cout<<"enter the second number ";
+    cin>>num2;
+    int result= maximum(num1,num2);
+ }
