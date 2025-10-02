89555e24af287b50fad17e3a79a70afb6b0ffa4e
Gautam88899 <gautam787075@gmail.com>
2025-10-02 14:42:07 +0530

    palidrone number program in c++

---
+++ palidrone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, rev = 0, rem, temp;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    temp = n;  
+
+    while (n > 0) {
+        rem = n % 10;        
+        rev = rev * 10 + rem;
+        n = n / 10;          
+    }
+
+    if (temp == rev)
+        cout << "Palindrome number";
+    else
+        cout << "Not a Palindrome number";
+
+    return 0;
+}
