8b16a797e81d8e6582e9cd2ef34df5b8e003d92d
Gautam88899 <gautam787075@gmail.com>
2025-10-03 15:21:59 +0530

    basin input in array withot loop

---
+++ arr1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    arr[0]=1;
+    arr[1]=6;
+    arr[2]=1;
+    arr[3]=5;
+    arr[4]=4;
+    
+    cout<<"the element in the array" << " "<<arr[0]<<" " <<arr[1]<<" " <<arr[2]<<" " <<arr[3]<<" " <<arr[4];
+}
