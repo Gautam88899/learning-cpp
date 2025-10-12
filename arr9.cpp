0e0f6c056887b7d573e360b350c9e29fe894b8e6
Gautam88899 <gautam787075@gmail.com>
2025-10-12 14:12:57 +0530

    basics array

---
+++ arr9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n],INT_MIN;
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int first = INT_MIN, second = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } 
+        else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == INT_MIN)
+        cout << "There is no second largest element." << endl;
+    else
+        cout << "The second largest element is: " << second << endl;
+
+    return 0;
+}
